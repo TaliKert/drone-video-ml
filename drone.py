d1bddf4666d76b0443211c9b141ff5f5fe78ede2
Kert Tali <kert@astri.ee>
2020-12-10 20:07:30 +0200

    Some reformatting

--- drone.py
+++ drone.py
@@ -1,5 +1,6 @@
 import argparse
 import sys
+
 # pull the Ultralytics "yolov3" library first
 sys.path.append("yolov3/")
 
@@ -9,11 +10,12 @@ from utils.utils import *
 from drone_command import *
 
 
-def detect(save_img=False):
+def detect():
     imgsz = opt.img_size
     out, source, weights, half, view_img, save_txt = opt.output, opt.source, opt.weights, opt.half, opt.view_img, opt.save_txt
-    #ip = opt.droneip
-    source = 'rtsp://'+"192.168.1.232"+':5554/camera'
+    # ip = opt.droneip
+    # For the Tello drone, this should be `udp://<LOCAL IP>:111111`
+    source = 'rtsp://' + "192.168.1.232" + ':5554/camera'
 
     # Initialize
     device = torch_utils.select_device(opt.device)
@@ -22,7 +24,6 @@ def detect(save_img=False):
     model = Darknet(opt.cfg, imgsz)
 
     # Load weights
-    attempt_download(weights)
     model.load_state_dict(torch.load(weights, map_location=device)['model'])
 
     # Eval mode
@@ -34,20 +35,23 @@ def detect(save_img=False):
         model.half()
 
     # Set Dataloader
-    view_img = True
     torch.backends.cudnn.benchmark = True  # set True to speed up constant image size inference
     dataset = LoadStreams(source, img_size=imgsz)
 
-    # Get names and colors
-    names = ["person"]
-    colors = [[random.randint(0, 255) for _ in range(3)] for _ in range(len(names))]
-
     # Run inference
     t0 = time.time()
+
+    run_inference(dataset, device, model, imgsz, half)
+
+    print('Done. (%.3fs)' % (time.time() - t0))
+
+
+def run_inference(dataset, device, model, imgsz, half):
     img = torch.zeros((1, 3, imgsz, imgsz), device=device)  # init img
     _ = model(img.half() if half else img.float()) if device.type != 'cpu' else None  # run once
 
-    for path, img, im0s, vid_cap in dataset:
+    # Once per frame
+    for path, img, im0s, _ in dataset:
         img = torch.from_numpy(img).to(device)
         img = img.half() if half else img.float()  # uint8 to fp16/32
         img /= 255.0  # 0 - 255 to 0.0 - 1.0
@@ -67,61 +71,49 @@ def detect(save_img=False):
         pred = non_max_suppression(pred, opt.conf_thres, opt.iou_thres,
                                    multi_label=False, classes=opt.classes, agnostic=opt.agnostic_nms)
 
-        bounding_boxes = []
-        # Process detections
-        for i, det in enumerate(pred):  # detections for image i
-            p, s, im0 = path[i], '%g: ' % i, im0s[i].copy()
+        bounding_boxes, im0 = process_detections(im0s, img, path, pred, t1, t2)
 
-            s += '%gx%g ' % img.shape[2:]  # print string
-            if det is not None and len(det):
-                # Rescale boxes from imgsz to im0 size
-                det[:, :4] = scale_coords(img.shape[2:], det[:, :4], im0.shape).round()
-
-                # Print results
-                for c in det[:, -1].unique():
-                    n = (det[:, -1] == c).sum()  # detections per class
-                    s += '%g %ss, ' % (n, names[int(c)])  # add to string
-
-                # Write results
-                for *xyxy, conf, cls in reversed(det):
-                    label = '%s %.2f' % (names[int(cls)], conf)
-                    plot_one_box(xyxy, im0, label=label, color=colors[int(cls)])
-
-                bounding_boxes.append(det[:, :4])
-
-            # Print time (inference + NMS)
-            print('%sDone. (%.3fs)' % (s, t2 - t1))
-
-            # Stream results
-            cv2.imshow(p, im0)
-            if cv2.waitKey(1) == ord('q'):  # q to quit
-                raise StopIteration
+        if im0 is not None and len(bounding_boxes) > 0:
+            sendCommandToDrone(bounding_boxes, im0.shape)
 
 
-        sendCommandToDrone(bounding_boxes, im0.shape)
+def process_detections(im0s, img, path, pred, t1, t2):
+    bounding_boxes = []
+    im0 = None
+    # Process detections
+    for i, det in enumerate(pred):  # detections for image i
+        p, s, im0 = path[i], '%g: ' % i, im0s[i].copy()
 
+        if det is not None and len(det):
+            s += f'{det.shape[0]:g} person(s) '
 
-    print('Done. (%.3fs)' % (time.time() - t0))
+            # Rescale boxes from imgsz to im0 size
+            det[:, :4] = scale_coords(img.shape[2:], det[:, :4], im0.shape).round()
+
+            # Write results
+            for *xyxy, conf, cls in reversed(det):
+                label = f'{conf:.2f}'
+                plot_one_box(xyxy, im0, label=label, color=(0, 255, 255))
+
+            bounding_boxes.append(det[:, :4])
+
+        # Print time (inference + NMS)
+        print(f'{s}Done. ({t2 - t1:.3f}s)')
+
+        # Stream results
+        cv2.imshow(p, im0)
+        if cv2.waitKey(1) == ord('q'):  # q to quit
+            raise StopIteration
+    return bounding_boxes, im0
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--cfg', type=str, default='cfg/yolov3-spp.cfg', help='*.cfg path')
-    parser.add_argument('--names', type=str, default='data/coco.names', help='*.names path')
-    parser.add_argument('--weights', type=str, default='weights/yolov3-spp-ultralytics.pt', help='weights path')
-    parser.add_argument('--source', type=str, default='data/samples', help='source')  # input file/folder, 0 for webcam
-    parser.add_argument('--output', type=str, default='output', help='output folder')  # output folder
+    parser.add_argument('--cfg', type=str, default='model/yolov3-tiny-person', help='*.cfg path')
+    parser.add_argument('--names', type=str, default='model/data/person-coco.names', help='*.names path')
+    parser.add_argument('--weights', type=str, default='rocket-preson-tiny.pt', help='weights path')
+    parser.add_argument('--source', type=str, default='192.168.0.101', help='source')
     parser.add_argument('--img-size', type=int, default=512, help='inference size (pixels)')
-    parser.add_argument('--conf-thres', type=float, default=0.3, help='object confidence threshold')
-    parser.add_argument('--iou-thres', type=float, default=0.6, help='IOU threshold for NMS')
-    parser.add_argument('--fourcc', type=str, default='mp4v', help='output video codec (verify ffmpeg support)')
-    parser.add_argument('--half', action='store_true', help='half precision FP16 inference')
-    parser.add_argument('--device', default='', help='device id (i.e. 0 or 0,1) or cpu')
-    parser.add_argument('--view-img', action='store_true', help='display results')
-    parser.add_argument('--save-txt', action='store_true', help='save results to *.txt')
-    parser.add_argument('--classes', nargs='+', type=int, help='filter by class')
-    parser.add_argument('--agnostic-nms', action='store_true', help='class-agnostic NMS')
-    parser.add_argument('--augment', action='store_true', help='augmented inference')
     parser.add_argument('--droneip', action='store_true', help='drone IP :D')
     opt = parser.parse_args()
     opt.cfg = check_file(opt.cfg)  # check file
