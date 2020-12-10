8826d93c8bc19d6fb21a97e250ed12ede5ea07df
Kert Tali <kert@astri.ee>
2020-12-10 20:23:13 +0200

    Fix errors regarding previous refactor

--- drone.py
+++ drone.py
@@ -11,37 +11,35 @@ from drone_command import *
 
 
 def detect():
-    imgsz = opt.img_size
-    out, source, weights, half, view_img, save_txt = opt.output, opt.source, opt.weights, opt.half, opt.view_img, opt.save_txt
     # ip = opt.droneip
     # For the Tello drone, this should be `udp://<LOCAL IP>:111111`
-    source = 'rtsp://' + "192.168.1.232" + ':5554/camera'
+    source = 'rtsp://' + opt.source + ':5554/camera'
 
     # Initialize
     device = torch_utils.select_device(opt.device)
 
     # Initialize model
-    model = Darknet(opt.cfg, imgsz)
+    model = Darknet(opt.cfg, opt.img_size)
 
     # Load weights
-    model.load_state_dict(torch.load(weights, map_location=device)['model'])
+    model.load_state_dict(torch.load(opt.weights, map_location=device)['model'])
 
     # Eval mode
     model.to(device).eval()
 
     # Half precision
-    half = half and device.type != 'cpu'  # half precision only supported on CUDA
+    half = device.type != 'cpu'  # half precision only supported on CUDA
     if half:
         model.half()
 
     # Set Dataloader
     torch.backends.cudnn.benchmark = True  # set True to speed up constant image size inference
-    dataset = LoadStreams(source, img_size=imgsz)
+    dataset = LoadStreams(source, img_size=opt.img_size)
 
     # Run inference
     t0 = time.time()
 
-    run_inference(dataset, device, model, imgsz, half)
+    run_inference(dataset, device, model, opt.img_size, half)
 
     print('Done. (%.3fs)' % (time.time() - t0))
 
@@ -60,7 +58,7 @@ def run_inference(dataset, device, model, imgsz, half):
 
         # Inference
         t1 = torch_utils.time_synchronized()
-        pred = model(img, augment=opt.augment)[0]
+        pred = model(img)[0]
         t2 = torch_utils.time_synchronized()
 
         # to float
@@ -68,8 +66,7 @@ def run_inference(dataset, device, model, imgsz, half):
             pred = pred.float()
 
         # Apply NMS
-        pred = non_max_suppression(pred, opt.conf_thres, opt.iou_thres,
-                                   multi_label=False, classes=opt.classes, agnostic=opt.agnostic_nms)
+        pred = non_max_suppression(pred, opt.conf_thres, opt.iou_thres, multi_label=False)
 
         bounding_boxes, im0 = process_detections(im0s, img, path, pred, t1, t2)
 
@@ -93,7 +90,7 @@ def process_detections(im0s, img, path, pred, t1, t2):
             # Write results
             for *xyxy, conf, cls in reversed(det):
                 label = f'{conf:.2f}'
-                plot_one_box(xyxy, im0, label=label, color=(0, 255, 255))
+                plot_one_box(xyxy, im0, label=label, color=(0, 137, 146))
 
             bounding_boxes.append(det[:, :4])
 
@@ -109,9 +106,12 @@ def process_detections(im0s, img, path, pred, t1, t2):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--cfg', type=str, default='model/yolov3-tiny-person', help='*.cfg path')
+    parser.add_argument('--cfg', type=str, default='model/yolov3-tiny-person.cfg', help='*.cfg path')
     parser.add_argument('--names', type=str, default='model/data/person-coco.names', help='*.names path')
-    parser.add_argument('--weights', type=str, default='rocket-preson-tiny.pt', help='weights path')
+    parser.add_argument('--weights', type=str, default='rocket-person-tiny.pt', help='weights path')
+    parser.add_argument('--conf-thres', type=float, default=0.3, help='object confidence threshold')
+    parser.add_argument('--iou-thres', type=float, default=0.6, help='IOU threshold for NMS')
+    parser.add_argument('--device', default='', help='device id (i.e. 0 or 0,1) or cpu')
     parser.add_argument('--source', type=str, default='192.168.0.101', help='source')
     parser.add_argument('--img-size', type=int, default=512, help='inference size (pixels)')
     parser.add_argument('--droneip', action='store_true', help='drone IP :D')
